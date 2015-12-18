51ca79fd10a794637825a6eccb6fce35f18379ed
tizz98 <dev.tizz98@gmail.com>
2015-12-18 17:15:36 -0500

    cleanup setup

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
-from setuptools import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with open('README.md') as f:
+    long_description = f.read()
 
 
 setup(
     name='netflix_roulette',
     version='0.1',
     description='A simple python wrapper for the Netflix Roulette API',
+    long_description=long_description,
     url='https://github.com/tizz98/netflix_roulette',
     author='Elijah wilson',
     author_email='elijah@elijahwilson.me',
